944f02056c90c9bf353a29d23eddc0d069b001fd
ayansh_1729 <cs21b2012@iiitdm.ac.in>
2024-04-02 15:21:45 +0530

    fixed about page

--- src/components/About_content.jsx
+++ src/components/About_content.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import "./About_content.css"
-import { Link } from 'react-router-dom';
-import img3 from "../assets/img2.jpeg"
+import React, { useState } from 'react';
+import "./About_content.css";
+import { Document, Page, pdfjs } from 'react-pdf';
+import resumePdf from "../assets/resume.pdf"; // Replace with the actual path to your resume PDF
+
+const PDFViewer = () => {
+  return (
+    <div className="pdf-container">
+      <iframe src={resumePdf} className="pdf-iframe" />
+    </div>
+  );
+};
 
 function About_content() {
   return (
-    <div className="about">
-        <div className="left">
-            <h1>Who Am I?</h1>
-            <p>I'm a react front-end developer.I create responsive secure websites for my clients</p>
-            <Link to="/contact" className='btn'>Contact</Link>
+    <div className="about flex">
+      <div className="resume__container flex">
+        <h1>Resume</h1>
+        <div className="my__line"></div>
+        <div className="quotes">
+          <p className="quote">"If your thirst is strong enough water isn't far away ..." - Ayansh</p>
+          <p className="quote"> - "Radhe Radhe" - </p>
+          {/* Add more quotes as needed */}
         </div>
-        <div className="right">
-            <div className="image__container">
-                <div className="img__top">
-                    <img src={img3} className='img'></img>
-                </div>
-                <div className="img__bottom">
-                    <img src={img3} className='img'></img>
-                </div>
-            </div>
-        </div>
+        <PDFViewer />
+      </div>
     </div>
-  )
+  );
 }
 
 export default About_content;
+
